54b9fd578dec9b39e5d2f8d8b429856e7756b533
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-13 22:41:47 +0400

    Draft

---
+++ README.md
@@ -0,0 +1,5 @@
+# Themis Finals sample Python checker cookbook
+Chef cookbook to provision Themis Finals sample Python checker. Part of [Themis Finals](https://github.com/aspyatkin/themis-finals) project.
+
+## License
+MIT @ [Alexander Pyatkin](https://github.com/aspyatkin)
---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+id = 'themis-finals-sample-checker-py'
+
+default[id][:basedir] = '/var/themis/finals/checkers/sample-checker-py'
+default[id][:repository] = 'aspyatkin/themis-finals-sample-checker-py'
+default[id][:revision] = 'master'
+default[id][:user] = 'vagrant'
+default[id][:group] = 'vagrant'
+
+default[id][:log_level] = 'DEBUG'
+default[id][:service_alias] = 'service_2'
+default[id][:processes] = 2
