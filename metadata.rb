46a699b15d2ff7c499d118483350b4679962a68f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-05-02 19:25:16 +0400

    Launch processes with Supervisor

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
 name 'themis-finals-sample-checker-py'
 description 'Installs and configures Themis Finals sample Python checker'
-version '1.1.2'
+version '1.1.3'
 
 recipe 'themis-finals-sample-checker-py', 'Installs and configures Themis Finals sample Python checker'
 depends 'latest-git', '~> 1.1.6'
 depends 'git2', '~> 1.0.0'
 depends 'python', '~> 1.4.6'
+depends 'supervisor', '~> 0.4.12'
+depends 'themis-finals', '~> 1.1.4'
