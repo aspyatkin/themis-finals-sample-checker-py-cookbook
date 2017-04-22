4892f2e1386c4010a8d0d15ad9854979422be4b9
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-04-22 17:36:14 +0400

    Update dependencies

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 id = 'themis-finals-service2-checker'
 
 default[id]['basedir'] = '/var/themis/finals/checker/service2'
-default[id]['github_repository'] = 'aspyatkin/themis-finals-service2-checker'
+default[id]['github_repository'] = 'themis-project/themis-finals-service2-checker'
 default[id]['revision'] = 'master'
 default[id]['user'] = 'vagrant'
 default[id]['group'] = 'vagrant'
